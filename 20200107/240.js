370bc5e66961d4de0e1716623a13063678609c27
me1a <mxh4373@gmail.com>
2020-01-07 14:03:33 +0800

    #240

--- 20200107/240.js
+++ 20200107/240.js
@@ -10,5 +10,32 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    
+    // for(let i of matrix) {
+    //     let l = i.length
+    //     if(i[0]===target || i[l-1] === target) return true
+    //     if(i[0] < target && i[l-1] >target) {
+    //         let start =0, end = i.length, mid = (end - start) >> 1
+    //         while(start < end && mid!==start && mid !== end) {
+    //             if(i[mid] > target){
+    //                 end = mid
+    //             } else if(i[mid]<target){
+    //                 start = mid
+    //             } else {
+    //                 return true
+    //             }
+    //             mid = ((end - start ) >> 1) + start
+    //         }
+    //     }   
+    // }
+    // return false
+
+    let row = matrix.length-1, col = 0
+    while(row >=0 && col < matrix[0].length) {
+        let that = matrix[row][col]
+        if(that === target) return true
+        else that < target? col++ : row--
+    }
+    return false
 };
+
+console.log(searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5))
